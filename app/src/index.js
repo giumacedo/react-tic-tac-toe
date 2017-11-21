e2525c5c4244154b87763358931eebd30919122d
Giulliano Macedo <giulliano.macedo@walmart.com>
2017-11-21 17:08:28 -0200

    add helper to check winner player

--- app/src/index.js
+++ app/src/index.js
@@ -38,8 +38,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
+    const winner = calculateWinner(this.state.squares);
+    console.log(winner);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+    }
+  
     return (
       <div>
         <div className="status">{status}</div>
@@ -77,6 +84,18 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+  for (let value of lines) {
+   const [a,b,c] = value;
+   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+     return squares[a];
+   }   
+  }
+  return null;
 }
 
 // ========================================
